ed24173cabaccc1a3d0f259cfc5e9e20e756c07b
Inbox Zero <team@inbox-zero.net>
2026-05-19 14:41:12 +0800

    feat(app/api/categories/[id]/route.ts): imported get controller and exported

--- app/api/categories/[id]/route.ts
+++ app/api/categories/[id]/route.ts
@@ -1,139 +1,14 @@
 import { 
-  NextResponse 
-} from "next/server";
-
-import { 
-  DbService
-} from "@/server/services/db-service"; 
-
-import { 
-  UpdateCategoryPayload 
-} from "@/types";
-
-import { 
-  headers 
-} from "next/headers";
-
-import { 
-  auth 
-} from "@/lib/auth";
+  GET
+} from "@/server/controllers/category-controller/category.id.controller"; 
 
 /**
- * =================================
- * GET /api/categories/[id]
- *
- * Fetches a single category by its ID.
- * =================================
+ * ====================================================================
+ * all the other grud operations has been moved to the parent route.ts 
+ * only GET is remaining here because that's standard convention 
+ * ====================================================================
  */
-export async function GET(
-  request: Request,
-  { params }: { params: Promise<{ id: string }> },
-) {
-  const session = await auth.api.getSession({ headers: await headers() });
 
-  if (!session) {
-    return NextResponse.json({ error: "Unauthenticated." }, { status: 401 });
-  }
-  try {
-    const { id } = await params;
-
-    const dbService = new DbService(); 
-    const client = await dbService.getDatabase(); 
-
-    const category = await client.category.findUnique({
-      where: { id },
-    });
-
-    if (!category) {
-      return NextResponse.json(
-        { error: "Category not found" },
-        { status: 404 },
-      );
-    }
-
-    return NextResponse.json(category);
-  } catch (error) {
-    console.error("Failed to fetch category:", error);
-    return NextResponse.json(
-      { error: "Failed to fetch category" },
-      { status: 500 },
-    );
-  }
-}
-
-/**
- * =================================
- * PUT /api/categories/[id]
- *
- * Updates an existing category.
- * =================================
- */
-export async function PUT(
-  request: Request,
-  { params }: { params: Promise<{ id: string }> },
-) {
-  const session = await auth.api.getSession({ headers: await headers() });
-
-  if (!session) {
-    return NextResponse.json({ error: "Unauthenticated." }, { status: 401 });
-  }
-  try {
-    const { id } = await params;
-    const { name }: UpdateCategoryPayload = await request.json();
-
-    const dbService = new DbService(); 
-    const client = await dbService.getDatabase(); 
-
-    const updatedCategory = await client.category.update({
-      where: { id },
-      data: {
-        name,
-      },
-    });
-
-    return NextResponse.json(updatedCategory);
-  } catch (error) {
-    console.error("Failed to update category:", error);
-    return NextResponse.json(
-      { error: "Failed to update category" },
-      { status: 500 },
-    );
-  }
-}
-
-/**
- * =================================
- * DELETE /api/categories/[id]
- *
- * Deletes a category by its ID.
- * =================================
- */
-export async function DELETE(
-  request: Request,
-  { params }: { params: Promise<{ id: string }> },
-) {
-  const session = await auth.api.getSession({ headers: await headers() });
-
-  if (!session) {
-    return NextResponse.json({ error: "Unauthenticated." }, { status: 401 });
-  }
-  try {
-    const { id } = await params;
-    
-    const dbService = new DbService(); 
-    
-    const client = await dbService.getDatabase();
-
-    await client.category.delete({
-      where: { id },
-    });
-
-    return new NextResponse(null, { status: 204 }); // No Content
-  } catch (error) {
-    console.error("Failed to delete category:", error);
-    return NextResponse.json(
-      { error: "Failed to delete category" },
-      { status: 500 },
-    );
-  }
+export { 
+  GET
 }
